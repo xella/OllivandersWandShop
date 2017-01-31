be2cd7cab575abf1dc89b86d0afbad97ee1105b4
Xella <xella.design@gmail.com>
2017-01-31 14:23:09 +0100

    Added horizontal and vertical ScrollViews. Added images for horizontal ScrollView. Added Ownership block.

--- app/src/main/java/com/example/xella/singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/xella/singlescreenapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
