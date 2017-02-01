ef89f6b0b5105b1cbfc0e1a49864ae0ae70f2a9b
Xella <xella.design@gmail.com>
2017-02-01 16:28:07 +0100

    Added method for checking if the place is opened now or closed.

--- app/src/main/java/com/example/xella/singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/xella/singlescreenapp/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.xella.singlescreenapp;
 
 import android.content.Intent;
-import android.location.Geocoder;
+import android.icu.util.Calendar;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openHoursCheck();
     }
 
     /**
@@ -62,5 +62,21 @@ public class MainActivity extends AppCompatActivity {
         if (searchAddress.resolveActivity(getPackageManager()) != null) {
             startActivity(searchAddress);
         }
+    }
+
+    /**
+     *  This method checking if the place is opened now or closed.
+     */
+    private void openHoursCheck() {
+        TextView openingHoursTextView = (TextView) findViewById(R.id.opening_hours);
+        String openingHoursText = openingHoursTextView.getText().toString();
+        Calendar currentDate = Calendar.getInstance();
+        int hours = currentDate.get(Calendar.HOUR_OF_DAY);
+        boolean isOpen = hours > 8 && hours < 22;
+        if (isOpen) {
+            openingHoursTextView.setText("Open now. " + openingHoursText);
+            return;
+        }
+        openingHoursTextView.setText("Closed now. " + openingHoursText);
     }
 }
